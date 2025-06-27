d1f49c2331d27b4ecd6d884f248b44f1582a2030
OGoriola <adedmiskal@gmail.com>
2025-06-26 20:24:19 -0600

    updated all

--- prac.c
+++ prac.c
@@ -3,8 +3,10 @@
 int main()
 {
     int new,old;
-    printf("Hello this is the first batch");
-    printf("This is modified");
+    printf("Hello this is the first batch\n");
+    printf("This is modified\n");
+    printf("Modified again\n");
+    getchar();
 
     return 0;
 }
