dc6f5f07cf388a6896e49e26a619e9a76c2c6fb9
Mickygee <adedmiskal@gmail.com>
2025-06-27 06:00:19 -0600

    corrected on github

--- prac.c
+++ prac.c
@@ -6,7 +6,7 @@ int main()
     system("color 0a");
     int new = 10;
     printf("Hello we are learning\n");
-    printf("This was edited in git/n");
+    printf("This was edited in git\n");
     printf("New number is: %d\n", new);
     getchar();
 
