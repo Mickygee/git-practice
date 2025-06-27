c6eb511ded775bebd0a842ffe71656ba446f996a
OGoriola <adedmiskal@gmail.com>
2025-06-26 23:04:42 -0600

    same line change

--- prac.c
+++ prac.c
@@ -5,7 +5,7 @@ int main()
 {
     system("color 0a");
     int new = 10;
-    printf("Hello this is the first batch\n");
+    printf("Hello we doing something\n");
     printf("This is for the branch/n");
     printf("New number is: %d\n", new);
     getchar();
